2aa5e96cb2aa0fe4350fc5c200c2d628c4dc05de
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 18:15:39 +0530

    Initial commit

---
+++ Q1.py
@@ -0,0 +1,13 @@
+# Q1
+
+import numpy as np
+
+arr_1D = np.array([10,20,30,40,50]) 
+print("1D Array :", arr_1D)
+print("data type :",arr_1D.dtype)
+print("Shape",arr_1D.shape)
+
+arr_2D = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("2D Array :", arr_2D)
+print("data type :",arr_2D.dtype)
+print("Shape",arr_2D.shape)
