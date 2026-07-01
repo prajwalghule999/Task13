14dd521a6db53bf6567d97905019430bff171889
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 19:38:09 +0530

    Complete Q6: Perform vector operations

--- Q6.py
+++ Q6.py
@@ -3,5 +3,7 @@ import numpy as np
 v1 = np.array([2, 4, 6, 8])
 v2 = np.array([1, 3, 5, 7])
 
-print("Addition of Matrix :")
-print(v1+v2)
+print("Addition of Matrix :",v1+v2)
+print("Subtraction of Matrix :",v1-v2)
+print("Multiplication of Matrix :",v1*v2)
+print("Dot Product of Matrix :",v1@v2)
