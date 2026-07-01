07e9918c42964eb1f0f59b178783d5e74fe2f8b7
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 18:42:12 +0530

    Complete Q2: Create arrays using zeros and ones

---
+++ Q2.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr1 = np.zeros(8)
+print("A 1D array of 8 zeros :",arr1)
+arr2 = np.ones(16).reshape(4,4)
+print("A 2D array of shape (4, 4) filled with ones :")
+print(arr2)
+arr3 = np.zeros(9).reshape(3,3)
+print("A 3x3 matrix of zeros :")
+print(arr3)
