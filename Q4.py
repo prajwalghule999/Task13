8a6186207463279846b1bfd10c593f766a63bedf
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 19:15:39 +0530

    Complete Q4: Generate arrays using linspace

---
+++ Q4.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.linspace(0,5,10)
+print("10 equally spaced numbers between 0 and 5 :")
+print(arr1)
+print("Length of Array :",len(arr1))
+
+arr2 = np.linspace(-10,10,15)
+print("15 equally spaced numbers between -10 and 10 :")
+print(arr2)
+print("Length of Array :",len(arr2))
