10a64f38428c134243f77f5a29453871d5c6c84a
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 20:12:30 +0530

    Complete Q9: Random array reshape and statistics

---
+++ Q9.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr = np.random.randint(1,50,20)
+print(arr)
+
+reshaped_arr = arr.reshape(4,5)
+print("\n Reshaped Array (4 x 5)")
+print(reshaped_arr)
+
+print("\n Sum of Matrix :", sum(reshaped_arr))
+
+print("\n Mean of Matrix :", np.mean(reshaped_arr))
+
+print("\n Standard deviation of Matrix :", np.std(reshaped_arr))
+
+# Maximum value in each row
+print("Maximum value in each row:")
+
+for i in reshaped_arr :
+    print(max(i))
