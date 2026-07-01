2066b548a986746e0c32e00f25414c99003f6f00
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 19:03:26 +0530

    Complete Q3: Generate arrays using arange

---
+++ Q3.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+arr1 = np.arange(0,21)
+print("Numbers from 0 to 20 :",arr1)
+
+arr2 = np.arange(10,50,2)
+print("Even numbers from 10 to 50 :",arr2)
+
+arr3 = np.arange(5,100,5)
+print("Numbers from 5 to 100 with step of 5 :",arr3)
