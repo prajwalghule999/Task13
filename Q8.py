bf7ae8ff4e564d03c4e6c8a0a71195a765027af8
Prajwal Ghule <prajwalghule999@gmail.com>
2026-07-01 19:55:48 +0530

    Complete Q8: Display array properties

---
+++ Q8.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.random.randint(1,101,(4,4))
+print(arr)
+
+print("\n Shape :",arr.shape)
+print("\n Dimension :",arr.ndim)
+print("\n Total number of elements (size) :",arr.size)
+print("\n Data type :",arr.dtype)
